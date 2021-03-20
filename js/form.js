aefd13e9c755094ea4a3a0201e0a7703bff12668
dahltina <71266815+dahltina@users.noreply.github.com>
2021-03-20 09:10:49 +0100

    client side form validation etc.

---
+++ js/form.js
@@ -0,0 +1,65 @@
+"use strict"
+
+const form = document.querySelector("#form");
+const firstName = document.querySelector("#first-name");
+const firstNameError = document.querySelector("#firstNameError");
+const lastName = document.querySelector("#last-name");
+const lastNameError = document.querySelector("#lastNameError");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const button = document.querySelector("#submit");
+const message = document.querySelector("#message");
+const messageError = document.querySelector("#messageError");
+const submitSuccess = document.querySelector("#success-message");
+
+form.addEventListener("submit", validateForm);
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+    event.preventDefault();
+
+    if (checkLength(firstName.value, 0) && checkLength(lastName.value, 0) && validateEmail(email.value) && checkLength(message, 19)) {
+        message.innerHTML = `<div class="success-message">Thank you! 
+                                Your message has been sent and we will get back to you shortly</div>`
+        form.reset();
+    }
+}
+
+function validateForm(event) {
+    event.preventDefault();
+
+    if (checkLength(firstName.value, 0)) {
+        firstNameError.style.display = "none";
+    } else {
+        firstNameError.style.display = "block";
+    }
+    if (checkLength(lastName.value, 0)) {
+        lastNameError.style.display = "none";
+    } else {
+        lastNameError.style.display = "block";
+    }
+    if (validateEmail(email.value)) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+    }
+    if (checkLength(message.value, 19)) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+    }
+}
+
+function checkLength(value, len) {
+    if (value.trim().length > len) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
---
+++ js/script.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// add to cart
+
+const addToCartbutton = document.querySelector("#add");
+const buyNowbutton = document.querySelector("#buyNow");
+const addedToCart = document.querySelector("#cart-add");
+const cartContainer = document.querySelector(".cart");
+const searchContainer = document.querySelector(".search");
+let itemsInCart = document.querySelector("#itemsInCart");
+
+let counter = 0;
+itemsInCart = counter;
+
+
+addToCartbutton.onclick = function () {
+    addedToCart.innerHTML = `<p>Added to cart!</p>
+                            <a href="mens.html">Keep shopping?</a>
+                            or <a href="checkout.html">Go to checkout</a>`;
+    addedToCart.classList.add("addedToCart");
+    addedToCart.classList.add("item-cart");
+    itemsInCart.classList.add("itemsInCart");
+    counter++;
+    //cartContainer.style.marginRight = "0px";
+    //searchContainer.style.marginLeft = "0px";
+}
+
+buyNowbutton.onclick = function () {
+    window.location.href = "checkout.html"
+}
+
+function addItemToCart() {
+    counter++;
+
+}
